a2fc01ed6a522177c4cfbcbf7e9821a3a3aa4fe0
ridhoabyaktaprayoga <ridhoabiyaktaprayoga13@gmail.com>
2025-03-17 01:42:47 +0700

    add: middleware for cors

---
+++ src/app/api/books/_middleware.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const response = NextResponse.next();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return response;
+}
